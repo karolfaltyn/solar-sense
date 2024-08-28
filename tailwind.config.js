f770408f61c26e5c3752fd3d80c89eff5618a0e2
Karol Faltyn <karolfaltyn03@gmail.com>
2024-08-28 19:55:48 +0200

    added radix ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        DEFAULT: "1300px",
+      },
+    },
+    extend: {
+      colors: {
+        finalBackground: "#F0F6F7",
+        background: "#FCFCFC",
+        gray: "#2A2A2A",
+        darkGray: "#161616",
+
+        blue: "#63CFEA",
+        darkBlue: "#009CD1",
+        green: "#79E381",
+        darkGreen: "#48C353",
+        yellow: "#F5DF41",
+        darkYellow: "#F0BF1E",
+        red: "#9C0000",
+        darkRed: "#4B0803",
+      },
+    },
   },
   plugins: [],
 };
